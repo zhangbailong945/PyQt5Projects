8cb471aa92388de958b09b1c3ca748ed72396a6a
zhangbailong945 <1207549344@qq.com>
2019-05-10 17:33:02 +0800

    am update

--- blogui/Ui_BottomCopyrightWidget.py
+++ blogui/Ui_BottomCopyrightWidget.py
@@ -8,54 +8,54 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_bottomCopyrightWidget(object):
-    def setupUi(self, bottomCopyrightWidget):
-        bottomCopyrightWidget.setObjectName("bottomCopyrightWidget")
-        bottomCopyrightWidget.resize(400, 41)
-        bottomCopyrightWidget.setAutoFillBackground(False)
-        bottomCopyrightWidget.setStyleSheet("background-color: rgb(17, 17, 17);\n"
+class Ui_BottomCopyrightWidget(object):
+    def setupUi(self, BottomCopyrightWidget):
+        BottomCopyrightWidget.setObjectName("BottomCopyrightWidget")
+        BottomCopyrightWidget.resize(400, 41)
+        BottomCopyrightWidget.setAutoFillBackground(False)
+        BottomCopyrightWidget.setStyleSheet("background-color: rgb(17, 17, 17);\n"
 "color: rgb(85, 85, 85);")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(bottomCopyrightWidget)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(BottomCopyrightWidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName("horizontalLayout")
         spacerItem = QtWidgets.QSpacerItem(60, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
-        self.label_BottomCopyright_Copyright = QtWidgets.QLabel(bottomCopyrightWidget)
+        self.label_BottomCopyright_Copyright = QtWidgets.QLabel(BottomCopyrightWidget)
         self.label_BottomCopyright_Copyright.setObjectName("label_BottomCopyright_Copyright")
         self.horizontalLayout.addWidget(self.label_BottomCopyright_Copyright)
-        self.label_BottomCopyright_Site = QtWidgets.QLabel(bottomCopyrightWidget)
+        self.label_BottomCopyright_Site = QtWidgets.QLabel(BottomCopyrightWidget)
         self.label_BottomCopyright_Site.setOpenExternalLinks(True)
         self.label_BottomCopyright_Site.setObjectName("label_BottomCopyright_Site")
         self.horizontalLayout.addWidget(self.label_BottomCopyright_Site)
-        self.label_BottomCopyRight_Line = QtWidgets.QLabel(bottomCopyrightWidget)
+        self.label_BottomCopyRight_Line = QtWidgets.QLabel(BottomCopyrightWidget)
         self.label_BottomCopyRight_Line.setObjectName("label_BottomCopyRight_Line")
         self.horizontalLayout.addWidget(self.label_BottomCopyRight_Line)
-        self.label_BottomCopyright_Beian = QtWidgets.QLabel(bottomCopyrightWidget)
+        self.label_BottomCopyright_Beian = QtWidgets.QLabel(BottomCopyrightWidget)
         self.label_BottomCopyright_Beian.setOpenExternalLinks(True)
         self.label_BottomCopyright_Beian.setObjectName("label_BottomCopyright_Beian")
         self.horizontalLayout.addWidget(self.label_BottomCopyright_Beian)
         spacerItem1 = QtWidgets.QSpacerItem(60, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
 
-        self.retranslateUi(bottomCopyrightWidget)
-        QtCore.QMetaObject.connectSlotsByName(bottomCopyrightWidget)
+        self.retranslateUi(BottomCopyrightWidget)
+        QtCore.QMetaObject.connectSlotsByName(BottomCopyrightWidget)
 
-    def retranslateUi(self, bottomCopyrightWidget):
+    def retranslateUi(self, BottomCopyrightWidget):
         _translate = QtCore.QCoreApplication.translate
-        bottomCopyrightWidget.setWindowTitle(_translate("bottomCopyrightWidget", "Form"))
-        self.label_BottomCopyright_Copyright.setText(_translate("bottomCopyrightWidget", "Copyright ©"))
-        self.label_BottomCopyright_Site.setText(_translate("bottomCopyrightWidget", "LoachBlog个人笔记"))
-        self.label_BottomCopyRight_Line.setText(_translate("bottomCopyrightWidget", "|"))
-        self.label_BottomCopyright_Beian.setText(_translate("bottomCopyrightWidget", "粤ICP备16026304号-2"))
+        BottomCopyrightWidget.setWindowTitle(_translate("BottomCopyrightWidget", "Form"))
+        self.label_BottomCopyright_Copyright.setText(_translate("BottomCopyrightWidget", "Copyright ©"))
+        self.label_BottomCopyright_Site.setText(_translate("BottomCopyrightWidget", "LoachBlog个人笔记"))
+        self.label_BottomCopyRight_Line.setText(_translate("BottomCopyrightWidget", "|"))
+        self.label_BottomCopyright_Beian.setText(_translate("BottomCopyrightWidget", "粤ICP备16026304号-2"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    bottomCopyrightWidget = QtWidgets.QWidget()
-    ui = Ui_bottomCopyrightWidget()
-    ui.setupUi(bottomCopyrightWidget)
-    bottomCopyrightWidget.show()
+    BottomCopyrightWidget = QtWidgets.QWidget()
+    ui = Ui_BottomCopyrightWidget()
+    ui.setupUi(BottomCopyrightWidget)
+    BottomCopyrightWidget.show()
     sys.exit(app.exec_())
 
