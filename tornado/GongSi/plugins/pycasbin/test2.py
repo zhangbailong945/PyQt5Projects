36638af2a29d03a60377c0bd18ebbcf11c658004
zhangbailong945 <1207549344@qq.com>
2019-07-11 17:46:09 +0800

    am

---
+++ tornado/GongSi/plugins/pycasbin/test2.py
@@ -0,0 +1,40 @@
+from mongodb_adapter.adapter import CasbinRule
+from mongodb_adapter.adapter import Adapter
+import casbin
+import os
+
+def get_enforcer(model=None,adapter=None,enable_log=False):
+    
+    return casbin.Enforcer(
+        model,
+        adapter,
+        enable_log
+    )
+
+def get_model():
+    return os.path.join(os.path.dirname(__file__)+"/rbac","rbac_model.conf").replace("\\","/")
+
+def get_adapter():
+    #return os.path.join(os.path.dirname(__file__)+"/rbac","rbac_policy.csv").replace("\\","/")
+    adapter=Adapter(dbname='gongsi',host='localhost',port=27017)
+    return adapter
+
+
+if __name__ == "__main__":
+    model=get_model()
+    adapter=Adapter(dbname='gongsi',host='localhost',port=27017)
+    e=get_enforcer(model,adapter)
+    #测试策略是否存在
+    print(e.enforce('admin', 'index', 'get'))
+    #输出用户的角色列表
+    print(e.get_roles_for_user('admin'))
+    #print(e.add_policy('zhangsan','index','post'))
+
+    #print(e.enforce('admin', 'index', 'post'))
+    #adapter=get_adapter()
+    #adapter.add_policy(sec=None,ptype='g',rule=['viewer','index','get'])
+    #e.add_policy('viewer','index','post') #ok
+
+    #print(e.get_all_roles()) #ok
+
+    #print(e.add_policy('zhangsan','index','post'))
